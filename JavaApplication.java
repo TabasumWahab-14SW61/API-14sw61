9814c0572ced598eaef5a5ef4dccf5b288a7395a
TabasumWahab-14SW61 <tabasum_swe@hotmail.com>
2017-06-07 22:27:31 +0500

    Update JavaApplication.java

--- JavaApplication.java
+++ JavaApplication.java
@@ -1,7 +1,7 @@
 
 
 														
-										//**************@author Tabasum Wahab - 14SW61******************//
+			//************** @Author--> Tabasum Wahab - 14SW61 ******************//
 
 		package javaApplication;
 
@@ -9,11 +9,8 @@
 
 
 			public class JavaApplication {
-
-				/**
-				* @param args the command line arguments
-				*/
-				// ************* API for addition of two numbers*****************
+				
+				// ************* API for addition of two numbers***************** //
 
 					/**
 					* This method is used to add two integer numbers.
@@ -21,9 +18,9 @@
 					   * num2  This is the second parameter to addNum method
 					   * @return int This returns sum of num1 and num2.
 					   */
-			public class AddNum {
+			public class AddTwoNum {
    
-				public int addNum(int num1, int num2) {
+				public int addTwoNum(int num1, int num2) {
 					return num1 + num2;
 				}
 
@@ -34,43 +31,42 @@
 				   */
 
 			public static void main(String args[]) throws IOException {
-				AddNum obj = new AddNum();
-				int sum = obj.addNum(25, 25);
+				AddTwoNum obj = new AddTwoNum();
+				int sum = obj.addTwoNum(25, 25);
 				System.out.println("Sum of 25 and 25 is :" + sum);
 			}
 			}
 
 
-				//API for the arrays put some values in the array, and prints each value to standard output
+		//API for the arrays put some values in the array, and prints each value to standard output
 
 
-				/** ArrayDemo, creates an array of integers, puts some values in the array, and prints each value to standard output. 
-				 * 
-				 * int[]anArray declares an array of integers. anArray = new int[10] allocates memory for 10 integers.  */
+		/** ArrayDemo, creates an array of integers, puts some values in the array, and prints each value to standard output. 
+		 **int[]anArray declares an array of integers. anArray = new int[10] allocates memory for 10 integers.  **/
 
 
 					class ArrayDemo {
 
 						public static void main(String[] args) {
 							// declares an array of integers
-							int[] anArray;
+							int[] MyArray;
 
 							// allocating memory for 5 integer numbers
-							anArray = new int[5];
+							MyArray = new int[5];
 							   
 							// initializing elements
-							anArray[0] = 100;
-							anArray[1] = 200;
-							anArray[2] = 300;
-							anArray[3] = 400;
-							anArray[4] = 500;
+							MyArray[0] = 100;
+							MyArray[1] = 200;
+							MyArray[2] = 300;
+							MyArray[3] = 400;
+							MyArray[4] = 500;
         
 
-								System.out.println("Element at index 0: "+ anArray[0]);
-								System.out.println("Element at index 1: " + anArray[1]);
-								System.out.println("Element at index 2: "+ anArray[2]);
-								System.out.println("Element at index 3: "+ anArray[3]);
-								System.out.println("Element at index 4: "+ anArray[4]);
+								System.out.println("Element at index 0: "+ MyArray[0]);
+								System.out.println("Element at index 1: " + MyArray[1]);
+								System.out.println("Element at index 2: "+ MyArray[2]);
+								System.out.println("Element at index 3: "+ MyArray[3]);
+								System.out.println("Element at index 4: "+ MyArray[4]);
      
 						}
 					}
